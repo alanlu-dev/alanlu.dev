22c73a4964736c8418846f81a1a745026afd17d6
Alan Lu <hi@alanlu.dev>
2024-11-14 12:16:41 +0800

    fix(website): nuxt 4 not yet

--- apps/website/nuxt.config.ts
+++ apps/website/nuxt.config.ts
@@ -1,7 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+  future: {
+    compatibilityVersion: 4,
+  },
+  // To re-enable _all_ Nuxt v3 behavior, set the following options:
+  srcDir: '.',
+  dir: {
+    app: 'app',
+  },
+  experimental: {
+    scanPageMeta: 'after-resolve',
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
   // https://nuxt.com/docs/api/configuration/nuxt-config#modules
   modules: [
     /* --- ui --- */
